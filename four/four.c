026dab24eb3a127b825d90606cb1fe282b24eb79
bachmeb <bachmeb@mail.uc.edu>
2016-03-10 14:43:45 -0500

    lesson 4

---
+++ four/four.c
@@ -0,0 +1,13 @@
+/* four.c  */
+#include <stdio.h>
+
+int main(){
+  int days;
+
+  days = 1;
+
+  printf("I've been programming C for %i day(s)\n",days);
+
+  return(1);
+}
+
---
+++ three/three.c
@@ -0,0 +1,12 @@
+/* one_day.c  */
+
+#include <stdio.h>
+
+int main(){
+
+  /* print statement */
+  printf("Keep on rockin in the free world.\n")
+
+  return(1);
+}
+
